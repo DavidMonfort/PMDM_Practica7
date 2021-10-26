5eaed1525b08a9878e2df8489320d50870a53907
David Monfort <dmonfortm@iesch.org>
2021-10-26 23:10:28 +0400

    Calculadora y Edad Canina funcionando

--- app/src/main/java/com/example/a6_dashboard/activities/Calculadora_Activity.java
+++ app/src/main/java/com/example/a6_dashboard/activities/Calculadora_Activity.java
@@ -2,15 +2,98 @@ package com.example.a6_dashboard.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a6_dashboard.R;
 
-public class Calculadora_Activity extends AppCompatActivity {
+public class Calculadora_Activity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnSum;
+    Button btnMin;
+    Button btnMult;
+    Button btnDiv;
+    View v;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        btnMin = findViewById(R.id.btnRestar);
+        btnSum = findViewById(R.id.btnSumar);
+        btnMult = findViewById(R.id.btnMult);
+        btnDiv = findViewById(R.id.btnDiv);
+
+        btnMin.setOnClickListener(this);
+        btnSum.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int buttonId = view.getId();
+        EditText numero1 = (EditText)findViewById(R.id.numberImput1);
+        EditText numero2 = (EditText) findViewById(R.id.numberImput2);
+        TextView result = (TextView) findViewById(R.id.txtResult);
+        double n1 = Integer.parseInt(numero1.getText().toString());
+        double n2 = Integer.parseInt(numero2.getText().toString());
+        double resultado =0;
+        if (numero1.equals("")){
+            Context context = getApplicationContext();
+            CharSequence text = "@string/number1Empty";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        if (numero2.equals("")){
+            Context context = getApplicationContext();
+            CharSequence text = "@string/number2Empty";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+
+        switch (buttonId){
+            case R.id.btnRestar:
+                resultado =  n1 - n2;
+                break;
+            case R.id.btnSumar:
+                resultado = n1 + n2;
+                break;
+            case R.id.btnMult:
+                resultado = n1 * n2;
+                break;
+            case R.id.btnDiv:
+                if (n2 == 0.0){
+                    Context context = getApplicationContext();
+                    CharSequence text = "@string/divAlert";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }else{
+                    resultado = n1 / n2;
+                    break;
+                }
+
+            default:
+                return;
+        }
+        String res = String.valueOf(resultado);
+        result.setText(res);
+
+
     }
 }
--- app/src/main/java/com/example/a6_dashboard/activities/EdadCanina_Activity.java
+++ app/src/main/java/com/example/a6_dashboard/activities/EdadCanina_Activity.java
@@ -2,7 +2,14 @@ package com.example.a6_dashboard.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.a6_dashboard.R;
 
@@ -12,5 +19,41 @@ public class EdadCanina_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edad_canina);
+
+        // 1 - Agregamos los ids a los elementos
+        EditText edadEdit = findViewById(R.id.edad_et);
+        TextView resultadoText =findViewById(R.id.resultado_tv);
+        Button boton = findViewById(R.id.button_btn);
+
+//        Log.v("MainActivity", "La actividad principal se ha creado");
+        Log.d("MainActivity", "La actividad principal se ha creado");
+//        Log.i("MainActivity", "La actividad principal se ha creado");
+//        Log.w("MainActivity", "La actividad principal se ha creado");
+//        Log.e("MainActivity", "La actividad principal se ha creado");
+
+
+        // 2- Damos actividad al boton al ser pulsado
+        boton.setOnClickListener( view -> {
+            // 3- Recogemos el texto del EditText
+            String edad = edadEdit.getText().toString();
+
+            if ( !edad.isEmpty() ){
+                //4 - Convertimo sese String a entero
+                int edadInt = Integer.parseInt(edad);
+                //5 - Calculamos la edad canina
+                int resultado = edadInt * 7;
+                // 6 - Creamos el texto que vamos a a mostrar
+                String resultadoString = getString(R.string.texto_resultado, resultado);
+                // 7 - Asignamos el texto al elemento TextView
+                resultadoText.setText(resultadoString);
+            } else {
+                Log.d("MainActivity","Has introducido un campo vacio");
+
+                Toast.makeText(this, getString(R.string.msg_toast),Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
     }
+
 }
